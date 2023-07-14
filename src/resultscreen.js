72f6915d9492a4f82e0d9ddaebfc47e35da81bed
Adrian Preisler <Adri.1103@web.de>
2023-07-14 19:41:30 +0200

    Name und Punkte im ResultScreen

--- src/resultscreen.js
+++ src/resultscreen.js
@@ -10,20 +10,13 @@ let correctAnswers = 0;
 getResultScreenUserInput();
 
 
-
-// User-Array / Time
-const daten = [
-  { spieler: "Peter", punkte: 100, zeit: "1:30" },
-  { spieler: "Anna", punkte: 50, zeit: "2:00" },
-];
-
 const test = [
   { UserInput: "Deutschland", Name: "Evo", Correct: "true" },
   { UserInput: "KA", Name: "Evo", Correct: "false" },
   { UserInput: "KA", Name: "Lemon", Correct: "true" }
 ]
 
-
+//endpunkt um das User Eingabe Array zu erhalten
 function getResultScreenUserInput() {
   fetch('/getResultScreenUserInput', {
     method: 'GET'
@@ -31,54 +24,49 @@ function getResultScreenUserInput() {
     .then(response => response.json())
     .then(data => {
       userTipps = data;
-      correct = getPoints(userTipps, userName);
-      console.log(correct);
-      console.log(userTipps);
+      var length = userTipps.length;
+
+      if (userTipps[length - 1].Name == userName) {
+        const tr = document.createElement("tr");
+
+        // Ergebnis-Button generieren
+        const resultCell = document.createElement("td");
+        const resultButton = document.createElement("button");
+        resultButton.textContent = "Mehr Informationen";
+        resultButton.addEventListener("click", () => {
+          window.location.href = "resultreview.html#" + userName;
+        });
+        resultCell.appendChild(resultButton);
+        tr.appendChild(resultCell);
+
+        // Spielername hinzufügen
+        const spielerCell = document.createElement("td");
+        spielerCell.textContent = userName;
+        tr.appendChild(spielerCell);
+
+        // Punkte hinzufügen
+        const punkteCell = document.createElement("td");
+        const punkte = getPoints(userTipps, userName); // Hier deine Funktion getPoints implementieren
+        punkteCell.textContent = punkte + "/15";
+        tr.appendChild(punkteCell);
+
+        // Zeit hinzufügen
+        const zeitCell = document.createElement("td");
+        const userTime = "00:30"; // Beispielzeit
+        zeitCell.textContent = userTime;
+        tr.appendChild(zeitCell);
+
+        tbody.appendChild(tr);
+      }
 
     });
 }
-
+//ließt die richtigen Punkte der Spieler aus
 function getPoints(userTipps, userName) {
   for (let i = 0; i < userTipps.length; i++) {
-    console.log(userTipps[i].Name == userName)
-    console.log(userTipps[i].Correct == true)
     if (userTipps[i].Name == userName && userTipps[i].Correct == true) {
       correctAnswers++;
     }
   }
-  console.log(correctAnswers);
   return correctAnswers;
-}
-
-
-
-
-
-// Elemente zur Tabelle hinzufügen
-daten.forEach((eintrag) => {
-  const tr = document.createElement("tr");
-
-  // Ergebnis-Zelle hinzufügen
-  const ergebnisCell = document.createElement("td");
-  const ergebnisButton = document.createElement("button");
-  ergebnisButton.textContent = "Ergebnis";
-  ergebnisButton.addEventListener("click", () => {
-    // Aktion ausführen, wenn auf den Ergebnis-Button geklickt wird
-    console.log("Weitere Informationen zum Ergebnis anzeigen");
-  });
-  ergebnisCell.appendChild(ergebnisButton);
-  tr.appendChild(ergebnisCell);
-
-  // Datenzellen hinzufügen
-  Object.values(eintrag).forEach((wert) => {
-    const td = document.createElement("td");
-    td.textContent = wert;
-    tr.appendChild(td);
-  });
-
-  tbody.appendChild(tr);
-});
-
-function ergebniss() {
-  window.location.href = "resultreview.html"
 }
